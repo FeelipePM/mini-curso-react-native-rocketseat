0ca98fb18b93533e43db6bae99e01f1f2558c5bb
Felipe <Feelipe@email.com>
2018-09-12 16:03:59 -0300

    Mini Curso React Native - Rocketseat
    Começando a aprender React

---
+++ react-native/mobile/App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Platform, TouchableOpacity, } from 'react-native';
+import Repo from './components/Repo';
+
+
+export default class App extends Component {
+
+  state = {
+    repos: [
+      {
+      id: 1,
+      thumbnail: 'https://avatars0.githubusercontent.com/u/30573043?s=460&v=4',
+      title: 'Github Felipe',
+      author: 'FelipePM',
+      },
+      {
+        id: 2,
+        thumbnail: 'https://avatars3.githubusercontent.com/u/4096456?s=460&v=4',
+        title: 'Github Marco Bruno',
+        author: 'MarcoBrunoBR',
+        },
+    ],
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Minicurso Gonative</Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={styles.headerButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView contentContainerStyle={styles.repoList}>  
+          { this.state.repos.map(repo => <Repo key={repo.id} data={repo} />) }
+        </ScrollView> 
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+
+  header: {
+    height: (Platform.OS === 'ios') ? 70 : 50,
+    paddingTop: (Platform.OS === 'ios') ? 30 : 0,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  headerButton: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  repoList: {
+    padding: 20,
+  },
+
+});
