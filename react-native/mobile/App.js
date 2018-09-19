077a51a3765474b379eaba7bc6bae2c5dfa77aba
Felipe <Feelipe@email.com>
2018-09-19 12:31:09 -0300

    Requisição à API e Storage - Mini Curso Finalizado!

--- react-native/mobile/App.js
+++ react-native/mobile/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, Platform, TouchableOpacity, } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Platform, AsyncStorage, TouchableOpacity, } from 'react-native';
 import Repo from './components/Repo';
 import NewRepoModal from './components/NewRepoModal';
 
@@ -16,27 +16,36 @@ export default class App extends Component {
 
   state = {
     modalVisible: false,
-    repos: [
-      {
-      id: 1,
-      thumbnail: 'https://avatars0.githubusercontent.com/u/30573043?s=460&v=4',
-      title: 'Github Felipe',
-      author: 'FelipePM',
-      },
-      {
-        id: 2,
-        thumbnail: 'https://avatars3.githubusercontent.com/u/4096456?s=460&v=4',
-        title: 'Github Marco Bruno',
-        author: 'MarcoBrunoBR',
-        },
-      {
-        id: 3,
-        thumbnail: 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
-        title: 'RocketSeat',
-        author: 'Diego3G',
-        },  
-    ],
+    repos: [],
   };
+
+  async componentDidMount() {
+    const repos = JSON.parse(await AsyncStorage.getItem('@Minicurso:repos')) || [];
+
+    this.setState({ repos });
+  }
+
+_addRepository = async (newRepoText) => {
+  const repoCall = await fetch(`http://api.github.com/repos/${newRepoText}`);
+  const response = await repoCall.json();
+
+  const repository = {
+    id: response.id,
+    thumbnail: response.owner.avatar_url,
+    title: response.name,
+    author: response.owner.login,
+  };
+
+  this.setState({
+    modalVisible: false,
+    repos: [
+      ...this.state.repos,
+      repository,
+    ],
+  });
+
+  await AsyncStorage.setItem('@Minicurso:repos', JSON.stringify(this.state.repos));
+};
 
   render() {
     return (
@@ -52,7 +61,10 @@ export default class App extends Component {
           { this.state.repos.map(repo => <Repo key={repo.id} data={repo} />) }
         </ScrollView> 
 
-        <NewRepoModal onCancel={() => this.setState({ modalVisible: false })} visible={this.state.modalVisible} />
+        <NewRepoModal onCancel={() => this.setState({ modalVisible: false })} 
+        onAdd={this._addRepository}
+        visible={this.state.modalVisible} 
+        />
       </View>
     );
   }
